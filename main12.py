c1a7f4837781ccbcf752dca0792dc4760143fac1
yeonsama <62858055+choimin1243@users.noreply.github.com>
2022-09-20 09:21:02 +0900

    Add files via upload

---
+++ main12.py
@@ -0,0 +1,35 @@
+
+import streamlit as st
+import pyautogui
+import pyperclip
+from time import sleep
+import pywinauto
+import re
+
+st.write("공문분석프로그램")
+checkbox = st.checkbox('agree')
+text = st.text_area("Your text:")
+
+btn_click=st.button("confirm",key='confirm_btn')
+
+win=pyautogui.getActiveWindow()
+
+
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
+
+m = p = re.compile('[a-z]+')
+
+
+if btn_click:
+    st.write(text)
+    list=text.split('\n')
+
+
+
+
+
+
+
+f=open("파일.txt","w", encoding='cp949')
+
